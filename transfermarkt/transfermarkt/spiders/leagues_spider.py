5ba4974d137874ff21f40203636fd5c46d9387ec
Mohammed <mohammedbehjooarchich@gmail.com>
2024-11-02 13:48:19 +0330

    get the league titles and urls

--- transfermarkt/transfermarkt/spiders/leagues_spider.py
+++ transfermarkt/transfermarkt/spiders/leagues_spider.py
@@ -1,11 +1,20 @@
 import scrapy
-
+from scrapy.linkextractors import LinkExtractor
+from scrapy.spiders import CrawlSpider, Rule
+from transfermarkt.items import TransfermarktItem
 
 class LeaguesSpiderSpider(scrapy.Spider):
     name = "leagues_spider"
     allowed_domains = ["www.transfermarkt.com"]
-    start_urls = ["https://www.transfermarkt.com/wettbewerbe/national/wettbewerbe/127"]
+    start_urls = [
+        "https://www.transfermarkt.com/wettbewerbe/national/wettbewerbe/127"]
+
 
     def parse(self, response):
-        LEAGUE_SELECTOR=".inline-table a"
-        
+        LEAGUE_SELECTOR = ".items .inline-table a"
+        LEAGUE_URL_SELECTOR = "::attr(href)"
+        for league in response.css(LEAGUE_SELECTOR):
+            item = TransfermarktItem()
+            item["league_name"]=league.css("::text").get()
+            item["league_url"]="https://www.transfermarkt.com" + league.css(LEAGUE_URL_SELECTOR).get()
+            yield item
