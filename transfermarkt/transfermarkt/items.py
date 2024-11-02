9c783e9435da82944feeb4181f6782fe1bde4ec5
Mohammed <mohammedbehjooarchich@gmail.com>
2024-11-02 12:04:11 +0330

    update settings.py file

--- transfermarkt/transfermarkt/items.py
+++ transfermarkt/transfermarkt/items.py
@@ -7,6 +7,5 @@ import scrapy
 
 
 class TransfermarktItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    league_name = scrapy.Field()
+    league_url = scrapy.Field()
--- transfermarkt/transfermarkt/spiders/leagues_spider.py
+++ transfermarkt/transfermarkt/spiders/leagues_spider.py
@@ -1,12 +1,11 @@
 import scrapy
 
 
-class TransfermarktSpiderSpider(scrapy.Spider):
-    name = "transfermarkt_spider"
+class LeaguesSpiderSpider(scrapy.Spider):
+    name = "leagues_spider"
     allowed_domains = ["www.transfermarkt.com"]
-    # for instance we work on Austria info for now: 127 is Austria
     start_urls = ["https://www.transfermarkt.com/wettbewerbe/national/wettbewerbe/127"]
 
     def parse(self, response):
+        LEAGUE_SELECTOR=".inline-table a"
         
-        pass
