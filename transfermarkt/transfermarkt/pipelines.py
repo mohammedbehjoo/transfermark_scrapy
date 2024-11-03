6e920959e7563b8ae0be8c23229787432a77dd75
Mohammed <mohammedbehjooarchich@gmail.com>
2024-11-03 09:42:51 +0330

    working on the pipelines for now

--- transfermarkt/transfermarkt/pipelines.py
+++ transfermarkt/transfermarkt/pipelines.py
@@ -6,7 +6,20 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from scrapy.exceptions import DropItem
+import re
+
 
 class TransfermarktPipeline:
+    def __init__(self):
+        self.league_names = set()
+
     def process_item(self, item, spider):
-        pass
+        # clean the league name by removing any whitespace
+        item["league_name"] = item["league_name"].strip()
+        
+        if re.match(r"\n", item["league_name"]):
+            raise DropItem("Duplicate item found: %s" % item)
+        else:
+            self.league_names.add(item["league_name"])
+            return item
