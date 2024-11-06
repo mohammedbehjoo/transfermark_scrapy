5bba4ee62279d9fe181998c806c07bba2e1fe9cb
Mohammed <mohammedbehjooarchich@gmail.com>
2024-11-06 12:07:53 +0330

    feat: get the teams for several seasons. you can change the range of seasons.

--- transfermarkt/transfermarkt/spiders/teams_spider.py
+++ transfermarkt/transfermarkt/spiders/teams_spider.py
@@ -1,14 +1,32 @@
 import scrapy
 from transfermarkt.items import TeamItem
+import pandas as pd
+
+# the dataframe for the scraped urls of the leagues crawled with leagues_spider
+df = pd.read_json(
+    "/home/mohammed/projects/coding/transfermarkt_scrapy/transfermarkt/transfermarkt/spiders/output.json")
+
+# create a list of league urls to iterate over them.
+urls_list=[]
+for league in df["leagues"]:
+    for item in league:
+        urls_list.append(item["league_url"])
 
 
 class TeamsSpiderSpider(scrapy.Spider):
+    # count the teams that are scraped
+    team_counter=0
+    
     name = "teams_spider"
     allowed_domains = ["www.transfermarkt.com"]
     start_urls = [
-        "https://www.transfermarkt.com/1-landesliga-niederosterreich/startseite/wettbewerb/A4N"]
+        "https://www.transfermarkt.com/"]
     
-    
+    def start_requests(self) :
+        for base_url in urls_list:
+            for season in range(2023,2025):
+                # add the saison_id to the url of the league_url
+                yield scrapy.Request(url=base_url+f"/plus/?saison_id={season}",callback=self.parse)    
 
     def parse(self, response):
         # print the response status
@@ -36,4 +54,7 @@ class TeamsSpiderSpider(scrapy.Spider):
                 f"Found team {team['team_name']}, Found squad size: {team['squad_size']}, avg age is: {team['avg_age']}, team url is: {team['team_url']}")
             print(
                 f"Number of foreigners is: {team['foreigners_num']}, avg market value is: {team['avg_market']}, and total market value is: {team['total_market']}")
+            self.team_counter+=1
+            self.logger.info(f"team counter is: {self.team_counter}")
+            print(f"team counter is: {self.team_counter}")
             yield team
