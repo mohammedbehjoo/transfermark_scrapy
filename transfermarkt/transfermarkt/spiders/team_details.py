3864124fa81d59ddb99ce9ea0a86bbd773d9c0b5
Mohammed <mohammedbehjooarchich@gmail.com>
2024-11-10 10:18:28 +0330

    feat: get the birth date of players. add it to the players dictionary. only get the birth date not the age.

--- transfermarkt/transfermarkt/spiders/team_details.py
+++ transfermarkt/transfermarkt/spiders/team_details.py
@@ -55,30 +55,51 @@ class TeamDetailsSpider(scrapy.Spider):
 
         # extract additional details here
         PLAYER_NAME_SELECTOR = ".items tbody td.posrela td.hauptlink a::text"
-        PLAYER_POSITION_SELECTOR=".items tbody  td.posrela tr td::text"
-        
+        PLAYER_POSITION_SELECTOR = ".items tbody  td.posrela tr td::text"
+        PLAYER_DATE_OF_BIRTH_SELECTOR = ".items td.zentriert"
         # response of the selectors
-        raw_names=response.css(PLAYER_NAME_SELECTOR).getall()
+        raw_names = response.css(PLAYER_NAME_SELECTOR).getall()
         raw_positions = response.css(PLAYER_POSITION_SELECTOR).getall()
 
         # Clean up the extracted player positions, and names by removing unwanted characters
-        cleaned_names=[na.strip() for na in raw_names if na.strip()]
-        cleaned_positions = [pos.strip() for pos in raw_positions if pos.strip()]
-        print(f"cleaned positions: {cleaned_positions}")
-        print(f"cleaned names: {cleaned_names}")
+        cleaned_names = [nam.strip() for nam in raw_names if nam.strip()]
+        cleaned_positions = [pos.strip()
+                             for pos in raw_positions if pos.strip()]
+
+        # extract date of birth elements
+        temp_list = []
+        dob_elements = response.css(PLAYER_DATE_OF_BIRTH_SELECTOR)[
+            1::2]  # Select every second element
+        for element in dob_elements:
+            text_content = element.css("::text").get()
+            if text_content:
+                temp_list.append(text_content.strip())
+        # only get every second item that is date of birth + age
+        dob_list = temp_list[::2]
+        # strip and split the raaw date of birth nag age. only get the dates.
+        dates_only_list = [item.split("(")[0].strip() for item in dob_list]
+        # # Print extracted dates of birth for verification
+        # print(f"Extracted dates of birth (every second element): {dob_texts}")
+        print(f"daets_only_list is: {dates_only_list}")
+        print(f"len dates_only_list is: {len(dates_only_list)}")
+
         # list of players
-        player_list=[]
-        for i,name in enumerate(cleaned_names):
-            position=cleaned_positions[i] if i < len(cleaned_positions) else None
-            print(f"Player {i + 1} - Name: {name}, Position: {position.strip() if position else 'N/A'}")
-            player_dict={
+        player_list = []
+        for i, name in enumerate(cleaned_names):
+            position = cleaned_positions[i] if i < len(
+                cleaned_positions) else None
+            date_of_birth = dates_only_list[i] if i < len(
+                dates_only_list) else None
+            # print(
+            #     f"Player {i + 1} - Name: {name}, Position: {position.strip() if position else 'N/A'}")
+            player_dict = {
                 "player_name": name,
-                "player_position": position.strip() if position else None
-                # "age":int(age.strip())
+                "player_position": position.strip() if position else None,
+                "date_of_birth": date_of_birth
             }
             player_list.append(player_dict)
         # add the player_list to the team_detail item
-        team_detail["players"] =player_list
+        team_detail["players"] = player_list
         print(f"extracted players with details: {player_list}")
 
         # yield the complete item after accumulating data from the second page
