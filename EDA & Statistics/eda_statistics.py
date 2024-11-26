be659bd505b306014d21b24c94e71212ec8cd211
Mohammed <mohammedbehjooarchich@gmail.com>
2024-11-26 19:40:40 +0330

    feat: read the tam.json file. create the df_teams dataframe. print the df_teams.describe(). save it to the .txt output file.

--- EDA & Statistics/eda_statistics.py
+++ EDA & Statistics/eda_statistics.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv
 import os
+from pathlib import Path
 import json
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -7,6 +8,9 @@ from matplotlib.ticker import MaxNLocator
 import seaborn as sns
 from scipy.stats import f_oneway,ttest_ind
 
+
+
+# a function to process and cast items of columns to float datatype
 def cast_float(item):
     if "€" in item:
         item = item.replace("€", "")
@@ -36,14 +40,36 @@ leagues = os.getenv("leagues")
 country_csv = os.getenv("country_csv")
 teams = os.getenv("teams")
 team_details = os.getenv("team_details")
+
+
 # directory for saving figures
-save_figure_dir=os.getenv("save_dir_figures")
+save_figure_leagues_dir=os.getenv("save_dir_figures")
+save_figure_leagues_dir=os.path.join(save_figure_leagues_dir,"leagues")
 # Create the directory if it does not exist
-os.makedirs(save_figure_dir, exist_ok=True)
-# directory for saving txt file
-save_txt_dir=os.getenv("save_dir_txt")
+os.makedirs(save_figure_leagues_dir, exist_ok=True)
+print("save figure leagues directory:\n",save_figure_leagues_dir)
+
+# directory for saving leagues txt file
+save_txt_leagues_dir=os.getenv("save_dir_txt")
+save_txt_leagues_dir=os.path.join(save_txt_leagues_dir,"leagues")
 # create the directory if it does not exists
-os.makedirs(save_txt_dir,exist_ok=True)
+os.makedirs(save_txt_leagues_dir,exist_ok=True)
+print("save txt leagues directory:\n",save_txt_leagues_dir)
+
+# directory for saving teams txt file
+save_txt_teams_dir=os.getenv("save_dir_txt")
+save_txt_teams_dir=os.path.join(save_txt_teams_dir,"teams")
+# create the teams directory if it does not exists
+os.makedirs(save_txt_teams_dir,exist_ok=True)
+print("save txt teams directory:\n",save_txt_teams_dir)
+
+# leagues txt file path
+leagues_txt_file_path=os.path.join(save_txt_leagues_dir,"results_df_league.txt")
+# teams txt file path
+teams_txt_file_path=os.path.join(save_txt_teams_dir,"teams.txt")
+
+
+
 
 # reading the lagues json file
 with open(leagues, "r") as file:
@@ -67,24 +93,24 @@ df_leagues["player_num"]=pd.to_numeric(df_leagues["player_num"],errors="coerce")
 print(f"describe df_leagues dataframe:\n{df_leagues.describe()}"+"\n"+"-"*30+"\n")
 
 # write the df_leagues.describe() to a txt file.
-txt_file_path=os.path.join(save_txt_dir,"results_df_league.txt")
-with open(txt_file_path,"a") as file:
+
+with open(leagues_txt_file_path,"a") as file:
     file.write("Describe df_leagues dataframe:\n")
     file.write(df_leagues.describe().to_string())
     file.write("\n"+"-"*30+"\n")
 
-print(f"df_leagues number.describe() is written to the file {txt_file_path}."+"\n"+"-"*30+"\n")
+print(f"df_leagues number.describe() is written to the file {leagues_txt_file_path}."+"\n"+"-"*30+"\n")
 
 
 # checking for missing values
 print(f"null values of df_leagues:\n{df_leagues.isnull().sum()}"+"\n"+"-"*30+"\n")
 
 # write the df_leagues null values count to a txt file.
-with open(txt_file_path,"a") as file:
+with open(leagues_txt_file_path,"a") as file:
     file.write("Number of null values of df_leagues dataframe columns:\n")
     file.write(df_leagues.isnull().sum().to_string())
     file.write("\n"+"-"*30+"\n")
-print(f"df_leagues number of null values is written to the file {txt_file_path}."+"\n"+"-"*30+"\n")
+print(f"df_leagues number of null values is written to the file {leagues_txt_file_path}."+"\n"+"-"*30+"\n")
 
 
 # distributions
@@ -95,41 +121,41 @@ for col in ["club_num","player_num","total_value"]:
     plt.title(f"Distirbution of {col}")
     plt.xlabel(col)
     plt.ylabel("Frequency")
-    fig_file_path=os.path.join(save_figure_dir,f"Disribution of {col}.jpg")
-    plt.savefig(fig_file_path,format="jpg")
+    leagues_fig_file_path=os.path.join(save_figure_leagues_dir,f"Disribution of {col}.jpg")
+    plt.savefig(leagues_fig_file_path,format="jpg")
     plt.close()
-    print(f"Figure is saved at: {fig_file_path}"+"\n"+"-"*30+"\n")
+    print(f"Figure is saved at: {leagues_fig_file_path}"+"\n"+"-"*30+"\n")
 
 # check skewness numerically
 print(f"skewness\n{df_leagues[['club_num','player_num','total_value']].skew()}"+"\n"+"-"*30+"\n")
 
 # write the df_leagues skewness to a txt file.
-with open(txt_file_path,"a") as file:
+with open(leagues_txt_file_path,"a") as file:
     file.write("skewness of df_leagues dataframe columns:\n")
     file.write(df_leagues[["club_num","player_num","total_value"]].skew().to_string())
     file.write("\n"+"-"*30+"\n")
-print(f"df_leagues skewness of columns is written to the file {txt_file_path}.","\n"+"-"*30+"\n")
+print(f"df_leagues skewness of columns is written to the file {leagues_txt_file_path}.","\n"+"-"*30+"\n")
 
 # correlations
 # correlation matrix
 correlation_matrix=df_leagues[["club_num","player_num","total_value"]].corr()
 print(f"Correlation matrix:\n{correlation_matrix}"+"\n"+"-"*30+"\n")
 # write the df_leagues correlation matrix to a txt file.
-with open(txt_file_path,"a") as file:
+with open(leagues_txt_file_path,"a") as file:
     file.write("correlation matrix of df_leagues dataframe columns:\n")
     file.write(correlation_matrix.to_string())
     file.write("\n"+"-"*30+"\n")
-print(f"df_leagues correlation matrix is written to the file {txt_file_path}."+"\n"+"-"*30+"\n")
+print(f"df_leagues correlation matrix is written to the file {leagues_txt_file_path}."+"\n"+"-"*30+"\n")
 
 # heatmap to visualize correlations
 # correlation matrix
 plt.figure(figsize=(8,6))
 sns.heatmap(correlation_matrix,annot=True,cmap="coolwarm",fmt=".2f")
 plt.title("Correlation matrix")
-fig_file_path=os.path.join(save_figure_dir,"correlation_matrix_df_leagues.jpg")
-plt.savefig(fig_file_path,format="jpg")
+leagues_fig_file_path=os.path.join(save_figure_leagues_dir,"correlation_matrix_df_leagues.jpg")
+plt.savefig(leagues_fig_file_path,format="jpg")
 plt.close()
-print(f"Figure is saved at: {fig_file_path}"+"\n"+"-"*30+"\n")
+print(f"Figure is saved at: {leagues_fig_file_path}"+"\n"+"-"*30+"\n")
 
 # outliers detection
 # box plots to identify outliers
@@ -139,10 +165,10 @@ for col in ["total_value","player_num","club_num"]:
     sns.boxplot(y=df_leagues[col],color="skyblue")
     plt.title(f"Boxplot of {col}")
     plt.ylabel(col)
-    fig_file_path=os.path.join(save_figure_dir,f"boxplot of {col}.jpg")
-    plt.savefig(fig_file_path,format="jpg")
+    leagues_fig_file_path=os.path.join(save_figure_leagues_dir,f"boxplot of {col}.jpg")
+    plt.savefig(leagues_fig_file_path,format="jpg")
     plt.close()
-    print(f"Figure is saved at: {fig_file_path}"+"\n"+"-"*30+"\n")
+    print(f"Figure is saved at: {leagues_fig_file_path}"+"\n"+"-"*30+"\n")
 
 # detecting outliers using IQR
 for col in ["total_value","player_num","club_num"]:
@@ -156,11 +182,11 @@ for col in ["total_value","player_num","club_num"]:
     outliers=df_leagues[(df_leagues[col]<lower_bound) | (df_leagues[col] > upper_bound) ]
     print(f"\nOutliers in {col}\n",outliers,"\n","-"*30,"\n")
     # write the df_leagues coutliers using IQR to a txt file.
-    with open(txt_file_path,"a") as file:
+    with open(leagues_txt_file_path,"a") as file:
         file.write(f"outliers of df_leagues dataframe of col {col}:\n")
         file.write(outliers.to_string())
         file.write("\n"+"-"*30+"\n")
-    print(f"df_leagues outliers are written to the file {txt_file_path}."+"\n"+"-"*30+"\n")
+    print(f"df_leagues outliers are written to the file {leagues_txt_file_path}."+"\n"+"-"*30+"\n")
     
 # calculate range for each numeric value
 for col in ["total_value","player_num","club_num"]:
@@ -170,13 +196,13 @@ for col in ["total_value","player_num","club_num"]:
 
     print(f"{col} -> Min: {col_min}, Max: {col_max}, Range: {col_range}"+"\n"+"-"*30+"\n")
     # write the df_leagues correlation matrix to a txt file.
-    with open(txt_file_path,"a") as file:
+    with open(leagues_txt_file_path,"a") as file:
         file.write(f"df_leagues dataframe range of col {col}:\n")
         file.write("Min: "+str(col_min)+"\n")
         file.write("Max: "+str(col_max)+"\n")
         file.write("Range: "+str(col_range)+"\n")
         file.write("\n"+"-"*30+"\n")
-    print(f"df_leagues range of column {col} is written to the file {txt_file_path}."+"\n"+"-"*30+"\n")
+    print(f"df_leagues range of column {col} is written to the file {leagues_txt_file_path}."+"\n"+"-"*30+"\n")
 
 # barplot to compare league values for each numeric column
 for col in ['club_num', 'player_num', 'total_value']:
@@ -186,10 +212,10 @@ for col in ['club_num', 'player_num', 'total_value']:
     plt.xlabel("League")
     plt.ylabel(col)
     plt.xticks(rotation=45)
-    fig_file_path=os.path.join(save_figure_dir,f"Comparison of {col} across leagues.jpg")
-    plt.savefig(fig_file_path,format="jpg")
+    leagues_fig_file_path=os.path.join(save_figure_leagues_dir,f"Comparison of {col} across leagues.jpg")
+    plt.savefig(leagues_fig_file_path,format="jpg")
     plt.close()
-    print(f"Figure is saved at: {fig_file_path}"+"\n"+"-"*30+"\n")
+    print(f"Figure is saved at: {leagues_fig_file_path}"+"\n"+"-"*30+"\n")
     
 # summary metric for each numeric column
 for col in ['club_num', 'player_num', 'total_value']:
@@ -199,14 +225,14 @@ for col in ['club_num', 'player_num', 'total_value']:
     print(f"Mode: {df_leagues[col].mode().iloc[0]:.2f}")
     print(f"Standard deviation: {df_leagues[col].std():.2f}")
     print("-"*30)
-    with open(txt_file_path,"a") as file:
+    with open(leagues_txt_file_path,"a") as file:
         file.write(f"Summary for {col}:"+"\n")
         file.write(f"Mean: {df_leagues[col].mean():.2f}"+"\n")
         file.write(f"Median: {df_leagues[col].median():.2f}"+"\n")
         file.write(f"Mode: {df_leagues[col].mode().iloc[0]:.2f}"+"\n")
         file.write(f"Standard deviation: {df_leagues[col].std():.2f}"+"\n")
         file.write("-"*30+"\n")
-        print(f"df_leagues summary metrics of column {col} are written to the file {txt_file_path}."+"\n"+"-"*30+"\n")
+        print(f"df_leagues summary metrics of column {col} are written to the file {leagues_txt_file_path}."+"\n"+"-"*30+"\n")
     
 # visualization of summary metrics
 for col in ['club_num', 'player_num', 'total_value']:
@@ -220,10 +246,10 @@ for col in ['club_num', 'player_num', 'total_value']:
     plt.title(f"{col} with mean and median")
     plt.ylabel(col)
     plt.legend()
-    fig_file_path=os.path.join(save_figure_dir,f"{col} with mean and meadian.jpg")
-    plt.savefig(fig_file_path,format="jpg")
+    leagues_fig_file_path=os.path.join(save_figure_leagues_dir,f"{col} with mean and meadian.jpg")
+    plt.savefig(leagues_fig_file_path,format="jpg")
     plt.close()
-    print(f"Figure is saved at: {fig_file_path}"+"\n"+"-"*30+"\n")
+    print(f"Figure is saved at: {leagues_fig_file_path}"+"\n"+"-"*30+"\n")
 
 # visualize overall ranges
 # calculate ranges
@@ -235,10 +261,10 @@ sns.barplot(x=ranges.values,y=ranges.index,palette="coolwarm")
 plt.title("Range of numeric columns")
 plt.xlabel("Range")
 plt.ylabel("Column")
-fig_file_path=os.path.join(save_figure_dir,"range of numeric columns.jpg")
-plt.savefig(fig_file_path,format="jpg")
+leagues_fig_file_path=os.path.join(save_figure_leagues_dir,"range of numeric columns.jpg")
+plt.savefig(leagues_fig_file_path,format="jpg")
 plt.close()
-print(f"Figure is saved at: {fig_file_path}"+"\n"+"-"*30+"\n")
+print(f"Figure is saved at: {leagues_fig_file_path}"+"\n"+"-"*30+"\n")
 
 # scatter plot: Club_num vs. total_value
 plt.figure(figsize=(8,6))
@@ -249,10 +275,10 @@ plt.ylabel("Total value",fontsize=12)
 plt.legend(title="League name",bbox_to_anchor=(1.05,1),loc="upper left")
 plt.grid(True)
 plt.tight_layout()
-fig_file_path=os.path.join(save_figure_dir,"Club number vs. Total value.jpg")
-plt.savefig(fig_file_path,format="jpg")
+leagues_fig_file_path=os.path.join(save_figure_leagues_dir,"Club number vs. Total value.jpg")
+plt.savefig(leagues_fig_file_path,format="jpg")
 plt.close()
-print(f"Figure is saved at: {fig_file_path}"+"\n"+"-"*30+"\n")
+print(f"Figure is saved at: {leagues_fig_file_path}"+"\n"+"-"*30+"\n")
 
 
 # Scatter plot: player_num vs. total_value
@@ -264,10 +290,10 @@ plt.ylabel('Total Value', fontsize=12)
 plt.legend(title='League Name', bbox_to_anchor=(1.05, 1), loc='upper left')
 plt.grid(True)
 plt.tight_layout()
-fig_file_path=os.path.join(save_figure_dir,"Player Number vs. Total Value.jpg")
-plt.savefig(fig_file_path,format="jpg")
+leagues_fig_file_path=os.path.join(save_figure_leagues_dir,"Player Number vs. Total Value.jpg")
+plt.savefig(leagues_fig_file_path,format="jpg")
 plt.close()
-print(f"Figure is saved at: {fig_file_path}"+"\n"+"-"*30+"\n")
+print(f"Figure is saved at: {leagues_fig_file_path}"+"\n"+"-"*30+"\n")
 
 # pie charts
 # categorical columns for leagues
@@ -283,10 +309,10 @@ plt.pie(
     colors=plt.cm.Paired.colors,    
 )
 plt.title("Prportion of players by league")
-fig_file_path=os.path.join(save_figure_dir,"Prportion of players by league.jpg")
-plt.savefig(fig_file_path,format="jpg")
+leagues_fig_file_path=os.path.join(save_figure_leagues_dir,"Prportion of players by league.jpg")
+plt.savefig(leagues_fig_file_path,format="jpg")
 plt.close()
-print(f"Figure is saved at: {fig_file_path}"+"\n"+"-"*30+"\n")
+print(f"Figure is saved at: {leagues_fig_file_path}"+"\n"+"-"*30+"\n")
 
 # pie chart for total_value
 plt.figure(figsize=(8,8))
@@ -296,8 +322,49 @@ plt.pie(df_leagues["total_value"],
         startangle=90,
         colors=plt.cm.Paired.colors)
 plt.title("Proportion of total value by league")
-fig_file_path=os.path.join(save_figure_dir,"Proportion of total value by league.jpg")
-plt.savefig(fig_file_path,format="jpg")
+leagues_fig_file_path=os.path.join(save_figure_leagues_dir,"Proportion of total value by league.jpg")
+plt.savefig(leagues_fig_file_path,format="jpg")
 plt.close()
-print(f"Figure is saved at: {fig_file_path}"+"\n"+"-"*30+"\n")
+print(f"Figure is saved at: {leagues_fig_file_path}"+"\n"+"-"*30+"\n")
 
+
+
+# ---------------------------
+# ---------------------------
+# teams dataframe
+
+# read the teams json file
+with open(teams,"r") as file:
+    if teams and os.path.exists(teams):
+        data=json.load(file)
+        print("teams json file is read.")
+    else:
+        print("the teams file is either missing or empty")
+        
+flattened_data=[]
+for league in data:
+    country_name=league["country_name"]
+    league_name=league["league_name"]
+    season=league["season"]
+    for team in league["teams"]:
+        team_data={"country_name":country_name,"league_name":league_name,"season":season,**team}
+        flattened_data.append(team_data)
+df_teams=pd.DataFrame(flattened_data)
+
+# cast dtypes to the appropriate dtypes
+df_teams["squad_size"]=pd.to_numeric(df_teams["squad_size"],errors="coerce")
+df_teams["avg_age"]=pd.to_numeric(df_teams["avg_age"],errors="coerce")
+df_teams["foreigners_num"]=pd.to_numeric(df_teams["foreigners_num"],errors="coerce")
+df_teams["avg_market"]=df_teams["avg_market"].apply(cast_float)
+df_teams["total_market"]=df_teams["total_market"].apply(cast_float)
+
+# basic summary statistics
+print(f"describe df_teams dataframe:\n{df_teams.describe()}"+"\n"+"-"*30+"\n")
+
+# write the df_teams.describe() to a txt file.
+with open(teams_txt_file_path,"a") as file:
+    file.write("Describe df_teams dataframe:\n")
+    file.write(df_teams.describe().to_string())
+    file.write("\n"+"-"*30+"\n")
+
+print(f"df_teams.describe() is written to the file {teams_txt_file_path}."+"\n"+"-"*30+"\n")
